dee7cfda2fe2bcdd6e0ee7f1a78eb8da9c9f1c77
SpiroSpica <hath1997@gmail.com>
2020-01-04 17:31:55 +0900

    Change xz plane to xy plane with z as depth

--- DefaultCreature.cs
+++ DefaultCreature.cs
@@ -5,13 +5,58 @@ using UnityEngine;
 public class DefaultCreature : MonoBehaviour
 {
     // Start is called before the first frame update
+    Transform creatureTransform;
+	bool side;
+	int laneNum;
+	float speed;
+	int hp;
+	float attackSpeed;
+	float attackRange;
+
     void Start()
     {
-        
+        creatureTransform = this.transform;
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+	void Instantiate(int lane, bool sideCheck, int creatureType)
+	{
+		
+		side = sideCheck;
+		laneNum = lane;
+		SetCreature(creatureType);
+		if (side)
+		{
+			switch (laneNum)
+			{
+				
+			}
+		}
+		else
+		{
+			speed *= -1;
+			switch (laneNum)
+			{
+				
+			}
+		}
+	}
+	void SetStartEnd(Vector3 start, Vector3 end)
+	{
+
+	}
+	void SetCreature(int creatureType)
+	{
+
+	}
+	void dead()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
     {
         
     }
