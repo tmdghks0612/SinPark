a26b681f0cff3a5b5b69b36c047cf5d71ec1ade1
SpiroSpica <hath1997@gmail.com>
2020-01-04 18:43:01 +0900

    CharacterMovement

--- DefaultCreature.cs
+++ DefaultCreature.cs
@@ -5,59 +5,92 @@ using UnityEngine;
 public class DefaultCreature : MonoBehaviour
 {
     // Start is called before the first frame update
-    Transform creatureTransform;
+    //Transform creatureTransform;
 	bool side;
 	int laneNum;
 	float speed;
 	int hp;
 	float attackSpeed;
 	float attackRange;
-
-    void Start()
+	Vector3 start;
+	Vector3 end;
+	bool moveFlag = true;
+	public GameControl gameControl;
+	void Start()
     {
-        creatureTransform = this.transform;
+		
+		attackSpeed = 2.0f;
+		side = false;
+		speed = 0.1f;
+		InvokeRepeating("DetectEnemy", 0.5f, attackSpeed);
+		start = new Vector2(10.0f, 0.0f);
+		end = new Vector2(-10.0f, 0.0f);
+		MoveTo(start, end);
+		
+		Instantiate(1, side, 1);
 
-    }
+	}
 
+	void MoveTo(Vector2 st, Vector2 ed)
+	{
+		start = st;
+		end = ed;
+		transform.position = start;
+	}
 
+	void DetectEnemy()
+	{
+		//moveFlag = GameControl.Detect();
+		Debug.Log("detect");
+	}
 	void Instantiate(int lane, bool sideCheck, int creatureType)
 	{
 		
 		side = sideCheck;
 		laneNum = lane;
-		SetCreature(creatureType);
-		if (side)
-		{
-			switch (laneNum)
-			{
-				
-			}
-		}
-		else
+		if(!side)
 		{
 			speed *= -1;
-			switch (laneNum)
-			{
-				
-			}
+			transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
 		}
 	}
-	void SetStartEnd(Vector3 start, Vector3 end)
-	{
 
+	void SetCreature()
+	{
+		
 	}
-	void SetCreature(int creatureType)
-	{
 
+	void DamageTaken(int damage)
+	{
+		hp -= damage;
+		if (hp <= 0)
+			dead();
 	}
 	void dead()
 	{
-
+		//GameControl.dead(this);
+		Destroy(this);
 	}
 
 	// Update is called once per frame
 	void Update()
     {
-        
+        if(moveFlag)
+		{
+			if(side && transform.position.x <= end.x)
+			{
+				moveFlag = true;
+				transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+			}
+			else if(!side && transform.position.x >= end.x)
+			{
+				moveFlag = true;
+				transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+			}
+			else
+			{
+				moveFlag = false;
+			}
+		}
     }
 }
