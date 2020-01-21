52f5180d918404978857bf70d0fd7779d7e624db
tmdghks0612 <dreamspidersh@gmail.com>
2020-01-21 14:19:15 +0900

    BoomerCreature script added

---
+++ CreatureScripts/BoomerCreature.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerCreature : DefaultCreature
+{
+    public GameObject explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void Attack()
+    {
+        if (attackFlag)
+        {
+            if (animControl != null)
+                animControl.SetBool("onAttack", true);
+            this.Dead();
+        }
+        else
+        {
+            if (animControl != null)
+                animControl.SetBool("onAttack", false);
+        }
+    }
+
+    protected override void Dead()
+    {
+        combatControl.PopCreature(laneNum, side, this);
+        CancelInvoke("Dead");
+        CancelInvoke("DetectEnemy");
+        combatControl.MeleeAttack(currentPosition, attackRange, attackDamage, laneNum, side);
+        Instantiate(explosion, currentPosition, transform.rotation = Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
---
+++ CreatureScripts/SpecialEffectScripts/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField]
+    protected float destroyDelay;
+    // Start is called before the first frame update
+
+    private void FixedUpdate()
+    {
+        Destroy(gameObject, destroyDelay);
+    }
+}
