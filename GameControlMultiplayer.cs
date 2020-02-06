e44527f245b85d3401daf179d3ab7b54ec815864
tmdghks0612 <dreamspidersh@gmail.com>
2020-02-06 17:18:08 +0900

    spawn request tested and error solved. now creatures can be spawned on multiplay environment

--- GameControlMultiplayer.cs
+++ GameControlMultiplayer.cs
@@ -9,13 +9,44 @@ using UnityEngine.Networking;
 
 public class GameControlMultiplayer : GameControl
 {
-
+    // queue for which spawn request waits
+    private Queue<SpawnRequestForm> spawnQueue = new Queue<SpawnRequestForm>();
     private int bufferSize = 1024;
+
+    //variable for synchronizing spawnQueue
+    private readonly object lock_spawn = new object();
+    //check rate for checking if queue is empty
+    private float checkRate = 0.01f;
+
     protected override void Start()
     {
         // call GameControl.Start()
         base.Start();
         aiplayer.AIplayerStop();
+        InvokeRepeating("CheckQueue", checkRate, checkRate);
+    }
+
+    // check queue if there are any creatures to spawn
+    public void CheckQueue()
+    {
+        lock (lock_spawn)
+        {
+            if (spawnQueue.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                SpawnRequestForm newRequestForm;
+                CancelInvoke("CheckQueue");
+                while (spawnQueue.Count != 0)
+                {
+                    newRequestForm = spawnQueue.Dequeue();
+                    spawnControl.SummonCreature(newRequestForm.GetlaneNum(), newRequestForm.GetSide(), newRequestForm.GetSelectedCreatureType());
+                }
+                InvokeRepeating("CheckQueue", checkRate, checkRate);
+            }
+        }
     }
 
     // Send spawn request of creature to server
@@ -37,6 +68,8 @@ public class GameControlMultiplayer : GameControl
             spawnRequestForm.SetlaneNum(_laneNum);
             spawnRequestForm.SetSelectedCreatureType(_selectedCreatureType);
             spawnRequestForm.SetSide(_side);
+
+            // create a JSON string with SpawnRequestForm instance
             string serverMessage = JsonUtility.ToJson(spawnRequestForm);
             buffer = Encoding.ASCII.GetBytes(serverMessage);
             serverControl.GetServerStream().Write(buffer, 0, buffer.Length);
@@ -54,9 +87,13 @@ public class GameControlMultiplayer : GameControl
     // create and set information of SpawnRequestForm
     public void ReceiveSpawnRequest(string jsonString)
     {
+        Debug.Log(jsonString);
         SpawnRequestForm newRequestForm = new SpawnRequestForm();
         newRequestForm = JsonUtility.FromJson<SpawnRequestForm>(jsonString);
-        spawnControl.SummonCreature(newRequestForm.GetlaneNum(), newRequestForm.GetSide(), newRequestForm.GetSelectedCreatureType());
+        lock (lock_spawn)
+        {
+            spawnQueue.Enqueue(newRequestForm);
+        }
         return;
     }
 
